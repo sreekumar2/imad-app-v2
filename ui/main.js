d101e2536b40947d3421c003398219924aa26232
sreekumar2 <srikmr42@gmail.com>
2017-02-10 21:23:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,11 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = "new value";
+
+//move the image
+var img = document.getElementById("sree");
+img.onclick = function () {
+    img.style.marginLeft = "100px" ;
+    
+    
+};
