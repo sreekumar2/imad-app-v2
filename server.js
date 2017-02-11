84021337fd1ac66370f02711f81b34f230fb696e
sreekumar2 <srikmr42@gmail.com>
2017-02-11 15:31:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,9 +93,9 @@ app.get('/counter', function (req,res) {
    res.send(counter.toString());
 });
 var names = [];
-app.get('/submit-name/:name',function (req,res){
+app.get('/submit-name',function (req,res){ //URL : /submit-name?name=xxxx
     //get the name from request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     //JASON= javascript object notation
