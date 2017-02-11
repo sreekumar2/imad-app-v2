1ee23351ee8010253ded0fc1fe85c47024a6caa4
sreekumar2 <srikmr42@gmail.com>
2017-02-11 16:11:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -41,9 +41,13 @@ img.onclick = function () {
       
   };
   // S ubmit name
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
   
   var submit = document.getElementById('submit_btn');
   submit.onclick = function(){
+      //create a request object
+      var request = new XMLHttpRequest();
       // capture the response and storw it in a variable
       request.onreadystatechange = function (){
           if (request.readyState === XMLHttpRequest.DONE) {
