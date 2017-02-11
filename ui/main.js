86d8ef1c2437a2f1aa5d44a14fc833e8cc11105d
sreekumar2 <srikmr42@gmail.com>
2017-02-11 15:12:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,4 +40,20 @@ img.onclick = function () {
      
       
   };
+  // S ubmit name
+  var nameInput = document.getElementById("name");
+  var name = nameInput.value;
+  var submit = document.getElementById('submit_btn');
+  submit.onclick = function(){
+      //make a request to the seerver and send the name'
+      //capture a list of names andf render it aas a lst
+      var names = ['name1','name2','name3','name4'];
+      var list ='';
+      for (var i=0; i < names.length; i++) {
+         list += '<li>' + names[i] + '</li>';
+          
+      }
+      var ul = document.getElementById('namelist');
+      ul.innerHTML = list;
+  };
  
